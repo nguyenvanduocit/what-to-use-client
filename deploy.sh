62c8d19a6e6a721db4930f8b2bcf4b99c9f9c7f1
Duoc Nguyen <nguyenvanduocit@gmail.com>
2017-09-18 23:47:24 +0700

    update graphql endpoint

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf .deploy
 mkdir .deploy
-GRAPHQL_URL=https://vue-curated-api.now.sh/graphql npm run build
+GRAPHQL_URL=https://what-to-use-chsbeaqpgs.now.sh/graphql npm run build
 mv dist .deploy/
 cp index.html .deploy/
 cp index.html .deploy/404.html
@@ -10,4 +10,4 @@ git init
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:vuejs/vue-curated-client.git master:gh-pages
+git push -f git@github.com:nguyenvanduocit/what-to-use-client.git master:gh-pages
